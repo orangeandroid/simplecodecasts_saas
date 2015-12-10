55e86e1d8e587e40b4b7d665c8eb629a06550e07
Axel Hawker <axelhawker@gmail.com>
2015-12-10 21:23:04 +0000

    Modified Devise for stripe

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,18 @@
+class Users::ResgistrationsController < Devise::ResgistrationsController
+    def create
+        #import the Devise create method so we can just add to it and not totally overwrite
+        super do |resource|
+            if params[:plan]
+                # add the plan_id to the users row in database
+                resource.plan_id = params[:plan]
+                #check which plan they signed up with
+                if resource.plan_id == 2
+                    
+                    resource.save_with_payment
+                else
+                    resource.save
+                end
+            end
+        end
+    end
+end
