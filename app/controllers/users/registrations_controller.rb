a7f691f156a4ed9baca656c355ca64d19258eafa
Axel Hawker <axelhawker@gmail.com>
2015-12-11 19:15:46 +0000

    Added notice styles and a notice if users try to sign up with a plan that doesn't exist

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+    
+    before_filter :select_plan, only: :new
+    
+    
+        
+    
+    
     def create
         #import the Devise create method so we can just add to it and not totally overwrite
         super do |resource|
@@ -15,4 +22,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
             end
         end
     end
+
+    private
+    
+        def select_plan
+            unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+                flash[:notice] = "Please select a membership plan to sign up"
+                redirect_to root_url
+        end
+    end
+
 end
+
